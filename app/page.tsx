3a49ef54bd8adfe74e674315cdf1c0560f0799f9
Aakshant Kumar <kumaraakshant2005@gmail.com>
2025-12-20 20:18:26 +0530

    Created the landing page for the protfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,100 @@
-import Image from "next/image";
+"use client";
+import { Playfair_Display, Dancing_Script } from "next/font/google";
+import Xarrow from "react-xarrows";
+import Link from "next/link";
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const ds = Dancing_Script({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-deep-space-blue-950">
+      <h1
+        className={`text-8xl font-semibold text-sky-100 ${ds.className} h-min text-center leading-[7rem] cursor-pointer`}
+      >
+        <span className="relative inline-block -left-10 hover:[text-shadow:var(--shadow-glow)]">
+          Are you{" "}
+        </span>
+        <span
+          id="elem1"
+          className="relative left-6 -top-2 inline-block transition-all duration-300 ease-in-out hover:text-cerulean-200 hover:rotate-[-10deg] hover:[text-shadow:var(--shadow-glow)]"
+        >
+          Curious?
+        </span>
+        <br />
+        <span
+          id="elem3"
+          className="inline-block relative transition-all duration-300 ease-in-out hover:text-cerulean-300 hover:-rotate-3 hover:[text-shadow:var(--shadow-glow)] top-6"
+        >
+          to{" "}
+        </span>
+        <Xarrow
+          start="elem1"
+          end="elem3"
+          curveness={3.5}
+          color="#89c7dc"
+          strokeWidth={3}
+          headSize={7}
+          passProps = {{strokeDasharray:"5, 2",}}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <span
+          id="elem2"
+          className="relative -top-2 inline-block left-10 transition-all duration-300 ease-in-out hover:text-cerulean-200 rotate-[-10deg] hover:rotate-[0deg] cursor-pointer hover:[text-shadow:var(--shadow-glow)]"
+        >
+          know
+        </span>
+        <br />
+        <span
+          id="elem4"
+          className="inline-block relative left-36 transition-all duration-300 ease-in-out hover:text-cerulean-300 rotate-3 hover:rotate-0 hover:[text-shadow:var(--shadow-glow)]"
+        >
+          oR
+        </span>{" "}
+        <br />
+        <span
+          id="elem5"
+          className="inline-block relative top-2 -left-44 transition-all duration-300 ease-in-out hover:text-cerulean-300 rotate-[-2.5deg] hover:rotate-0"
+        >
+          <Link
+            href="/about"
+            className="hover:[text-shadow:var(--shadow-glow)]"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+            unexperiencedone?
+          </Link>
+        </span>
+        <Xarrow
+          start="elem4"
+          end="elem5"
+          curveness={3.5}
+          color="#89c7dc"
+          strokeWidth={3}
+          headSize={7}
+        />
+        <span
+          id="elem6"
+          className="inline-block -right-24 relative top-2 transition-all duration-300 ease-in-out hover:text-cerulean-300 rotate-[5deg] hover:rotate-0 hover:[text-shadow:0_4px_8px_rgba(0,0,0,0.3)]"
+        >
+          <Link href="/work" className="hover:[text-shadow:var(--shadow-glow)]">
+            's work
+          </Link>
+        </span>
+        <Xarrow
+          start="elem4"
+          end="elem6"
+          curveness={3.5}
+          color="#89c7dc"
+          strokeWidth={3}
+          headSize={7}
+          passProps={{strokeDasharray: "5, 2",}}
+        />
+      </h1>
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,91 @@
+// components/Navbar.tsx
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Dancing_Script, Playfair_Display, Inter } from "next/font/google";
+import { time } from "console";
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const ds = Dancing_Script({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Blogs", href: "/blogs" },
+  { name: "Articles", href: "/articles" },
+  { name: "Services", href: "/services" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(()=>{
+          const threshold = window.innerHeight * 0.125;
+          setIsScrolled(window.scrollY > threshold);
+      }, 10)
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+        window.removeEventListener("scroll", handleScroll);
+        clearTimeout(timeoutId)
+    };
+  }, []);
+
+  return (
+    <header className="h-min">
+      <nav className={`fixed left-1/2 -translate-x-1/2 transition-all duration-700 cubic-bezier(0.4, 0, 0.2, 1) z-50 flex items-center justify-between
+          ${isScrolled 
+            ? "top-4 w-[90%] max-w-4xl rounded-2xl bg-deep-space-blue-700/80 backdrop-blur-md shadow-lg px-8 py-3 border border-light-blue-300" 
+            : "top-0 w-full bg-deep-space-blue-900 px-6 py-4 border-b border-transparent"
+          }`}>
+        <h1 className={`text-3xl font-bold ${ds.className} text-air-force-blue-400`}>
+          Unexperiencedone
+        </h1>
+
+        <ul className="flex gap-15">
+          {links.map((link) => {
+            const isActive = pathname === link.href;
+            return (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={`relative pb-1 transition-colors duration-200 
+                after:absolute after:left-0
+                after:-bottom-1 after:h-[2px] after:w-full after:bg-rich-cerulean-500
+                after:scale-x-0 after:origin-left 
+                after:transition-transform after:duration-300
+                hover:after:scale-x-100 ${playfair.className}
+                ${
+                  isActive
+                    ? "text-yale-blue-300 after:scale-x-100"
+                    : "text-gray-500 hover:text-yale-blue-300"
+                }`}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
